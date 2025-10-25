cddace9e4ceba92a07331e9e4be6910ff939f289
Bryce Dunlap <brycedunlap@bryces-mbp.mynetworksettings.com>
2025-10-24 23:30:19 -0400

    new and improved version

--- README.txt
+++ README.txt
@@ -1,1 +1,19 @@
 teststingtestingn123
+
+npm install nodemailer
+
+will set up better package management if grows
+
+
+
+
+Then set:
+Setting	Value
+Device name	Galaxy Tab A9+ 5G
+Width	800
+Height	1280
+Device pixel ratio (DPR)	1.5
+User agent string	Mozilla/5.0 (Linux; Android 13; SM-X216B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36
+Touch	Enabled
+Mobile	Enabled
+Orientation	Portrait (you can flip to landscape at 1280 × 800)
---
+++ led-controller.js
@@ -0,0 +1,19 @@
+    
+    // post requst for the OLED
+    /*
+    * TODO: Find which POST requests go with which feelings
+    */
+
+
+    document.getElementById("oledBtn").addEventListener("click", async ()=>{
+    try {
+      await fetch("http://192.168.0.65/json/state", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ on: "t"})
+      });
+      console.log("Payload sent to WLED");
+    } catch(err) {
+      console.log("POST failed:", err);
+    }
+  });
