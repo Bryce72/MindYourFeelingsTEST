585f902aed7a07a3270f22819be938ca93bd79e8
Bryce Dunlap <brycedunlap@bryces-mbp.mynetworksettings.com>
2025-10-29 23:58:30 -0400

    weird somethings were deleted

--- led-controller.js
+++ led-controller.js
@@ -1,19 +1,32 @@
+function DoSomething(){
+    window.alert = function () { };
+
+    // Use SweetAlert to display an alert
+    Swal.fire({
+        title: 'Hows the page look on the tablet??',
+        icon: 'question',
+        confirmButtonText: 'OK'
+    });
+}
+    
+    
+    
+    
+    
     
     // post requst for the OLED
     /*
     * TODO: Find which POST requests go with which feelings
     */
-
-
-    document.getElementById("oledBtn").addEventListener("click", async ()=>{
-    try {
-      await fetch("http://192.168.0.65/json/state", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ on: "t"})
-      });
-      console.log("Payload sent to WLED");
-    } catch(err) {
-      console.log("POST failed:", err);
-    }
-  });
+  //   document.getElementById("oledBtn").addEventListener("click", async ()=>{
+  //   try {
+  //     await fetch("http://192.168.0.65/json/state", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({ on: "t"})
+  //     });
+  //     console.log("Payload sent to WLED");
+  //   } catch(err) {
+  //     console.log("POST failed:", err);
+  //   }
+  // });
